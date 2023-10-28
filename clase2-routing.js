111988c3110bbb935db13369aeb7daaa367a8cd3
yayobyte <yayobyte@gmail.com>
2023-10-28 13:01:43 +0400

    routing

--- clase2-routing.js
+++ clase2-routing.js
@@ -2,8 +2,43 @@ const http = require('node:http')
 
 const desiredPort = process.env.PORT ?? 3000
 
-const processRequest = (req, res) => {
+const dittoJSON = require('./ditto.json')
 
+const processRequest = (req, res) => {
+    const { method, url } = req
+
+    switch (method) {
+        case 'GET':
+            switch (url) {
+                case '/pokemon/ditto':
+                    res.setHeader('Content-Type', 'application/json; charset=utf8')
+                    return res.end(JSON.stringify(dittoJSON))
+                default:
+                    res.statusCode = 404
+                    res.setHeader('Content-Type', 'text/html; charset=utf8')
+                    return res.end('<h1>Not found</h1>')
+            }
+        case 'POST':
+            let body = ''
+            switch (url) {
+                case '/pokemon/ditto':
+                    req.on('data', (chunk) => {
+                        body += chunk.toString()
+                    })
+                    req.on('end', () => {
+                        const data = JSON.parse(body)
+                        res.writeHead(201, { 'Content-Type': 'application/json; charset=utf8'})
+                        res.end(JSON.stringify(data))
+                    })
+                    return
+                default:
+                    res.statusCode = 404
+                    res.setHeader('Content-Type', 'text/html; charset=utf8')
+                    return res.end('<h1>Not found</h1>')
+
+            }
+
+    }
 }
 
 const server = http.createServer(processRequest)
